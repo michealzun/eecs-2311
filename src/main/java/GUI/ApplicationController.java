15465f66e05c06311cae8dbd82503d0a01dba15d
michealzun <michealzun@gmail.com>
2022-02-20 21:00:00 -0500

    undo

--- src/main/java/GUI/ApplicationController.java
+++ src/main/java/GUI/ApplicationController.java
@@ -45,10 +45,7 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
-import parser.Measure;
-import parser.Note;
-import parser.Parser;
-import parser.Part;
+
 
 public class ApplicationController implements Initializable {
 
@@ -123,108 +120,11 @@ public class ApplicationController implements Initializable {
 
 	// PDF Attributes	
 
-	// Initialization Phase
-	static int measureBarLength = 32;
-	static int measureWidth=400;
-	static int measureSpacing=100;
-	static int measuresPerLine=4;
-	String input="";
-
-	static void drawMeasure(int measureNumber, Measure m) {
-
-		String measureType="";
-		switch(m.lines.size()) {
-		case 1:
-			measureType="\uD834\uDD16";
-			break;
-		case 2:
-			measureType="\uD834\uDD17";
-			break;
-		case 3:
-			measureType="\uD834\uDD18";
-			break;
-		case 4:
-			measureType="\uD834\uDD19";
-			break;
-		case 5:
-			measureType="\uD834\uDD1A";
-			break;
-		case 6:
-			measureType="\uD834\uDD1B";
-			break;
-		}
-
-		for(int i = 0; i < measureBarLength; i++) {
-			drawString(measureType, 50 + measureWidth * (measureNumber % measuresPerLine) + i * 30, 40 + measureSpacing * (measureNumber / measuresPerLine));
-		}
-
-		for (int i = 0; i < m.notes.size(); i++)  {
-			drawNote(50 + measureWidth * (measureNumber % measuresPerLine), 40 + measureSpacing * (measureNumber / measuresPerLine), m.notes.get(i));
-		}
-	}
-
-	static void drawNote(int noteX, int noteY, Note n) {
-		// Note Location
-		int x = noteX + n.duration; 
-		int y = noteY; 
-
-		//WIP
-
-		// Note Type
-		String noteType="";
-		switch(n.type) {
-		case "whole":
-			noteType="\uD834\uDD5D";
-			break;
-		case "half":
-			noteType="\uD834\uDD5E";
-			break;
-		case "quarter":
-			noteType="\uD834\uDD5F";
-			break;
-		case "eighth":
-			noteType="\uD834\uDD60";
-			break;
-		case "sixteenth":
-			noteType="\uD834\uDD61";
-			break;
-		case "thirtysixth":
-			noteType="\uD834\uDD62";
-			break;
-		}
-		drawString(noteType, x, y);
-	}
-
-	private static void drawString(String noteType, int x, int y) {
-		JFrame f = new JFrame() {
-			public void paint(Graphics g) {
-				Graphics2D g2 = (Graphics2D) g;
-				g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
-						RenderingHints.VALUE_ANTIALIAS_ON);
-
-				Font font = new Font("Bravura", Font.PLAIN, 32);
-
-				g2.setFont(font);
-				g2.drawString(noteType, x, y);
-
-			}
-		};
-		f.setSize(200,200);
-		f.setVisible(true);
-	}
 	
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		this.parser = new Parser();
-		parser.setInput(input);
-		Part p = parser.getSheetInfo().get(0); // only do the first instrument for now
-		List<Measure> measures = p.measures;
-
-		for (int i = 0; i < measures.size(); i++) {
-			drawMeasure(measures.get(i).number, measures.get(i));
-		}
-		
 	}
 
 	// WIP
